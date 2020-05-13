64f26d61258bb6e9b22251c5d3d6dac881c9f5e9
chunghwanyoon <chunghwan@redrocket.co.kr>
2020-05-13 14:36:05 +0900

    add initializing error handlings

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as bodyParser from "body-parser";
 import * as mongoose from "mongoose";
 import Controller from "./interfaces/controller.interface";
+import errorMiddleware from "./middleware/error.middleware";
 
 class App {
   public app: express.Application;
@@ -12,9 +13,14 @@ class App {
     this.connectToTheDatabase();
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
+    this.initializeErrorHandling();
   }
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
   }
 
   private initializeControllers(controllers: Controller[]) {
