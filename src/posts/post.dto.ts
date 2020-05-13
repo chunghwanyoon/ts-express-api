af1004e03cd070d842c69345b287a1aa8cb1845b
chunghwanyoon <chunghwan@redrocket.co.kr>
2020-05-13 14:35:33 +0900

    add data transfer object class

---
+++ src/posts/post.dto.ts
@@ -0,0 +1,15 @@
+// DTO means data transfer object
+import { IsString } from "class-validator";
+
+class CreatePostDto {
+  @IsString()
+  public author: string;
+
+  @IsString()
+  public content: string;
+
+  @IsString()
+  public title: string;
+}
+
+export default CreatePostDto;
